8376dfb91c70ca493ed65ffcad563a3a9fbe8823
ChannaMaduranga <channamaduranga24@gmail.com>
2021-05-11 01:22:53 +0530

    nbbfhdfjd

---
+++ form.php
@@ -0,0 +1,30 @@
+<?php
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$subject = $_POST['subject'];
+4$message = $_POST['message'];
+
+  
+$email_from = 'info.wskv@gmail.com';
+
+$email_subject = 'New Form Submission';
+
+$email_body = "User Name: $name.\n".
+				"User Email: $visitor_email.\n".
+				"Subject: $subject.\n".
+				"|User Message: $message .\n";
+
+$to = 'channamaduranga24@gmail.com';
+
+$headers = "From: $email_from \r\n";
+
+$headers .= "Reply-To: $visitor_email \r\n";
+
+mail($to,$email_subject,$email_body,$headers);
+
+header("Location: Contact.html");
+
+?>
+
+
+
